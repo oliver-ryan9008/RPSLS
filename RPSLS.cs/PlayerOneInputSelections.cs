94ee82a20d42865851bf14261b1ab359a37db291
oliver.ryan9008 <38868314+rdo1708@users.noreply.github.com>
2018-06-20 17:12:04 -0500

    Fixed github file paths

---
+++ RPSLS.cs/PlayerOneInputSelections.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS.cs
+{
+    class PlayerOneInputSelections
+    {
+        // member variables(has a)
+        public object playerOneChoice;
+
+        // constructor
+        public PlayerOneInputSelections(object playerOneChoice)
+        {
+            this.playerOneChoice = playerOneChoice;
+        }
+
+        // member methods
+    }
+}
---
+++ RPSLS.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you ready to play?");
+
+            if (Console.ReadLine().ToLower() == "yes")
+            {
+                Console.WriteLine("Player 1, enter your selection here. You may choose rock, paper, scissors, lizard, or spock.");
+                string userOneInputSelections = Console.ReadLine();
+                PlayerOneInputSelections playerOneChoiceAnswer = new PlayerOneInputSelections(userOneInputSelections);
+
+                Console.WriteLine("Player 2, enter your selection here. You may choose rock, paper, scissors, lizard, or spock.");
+                string userTwoInputSelections = Console.ReadLine().ToLower();
+                PlayerTwoInputSelections playerTwoChoiceAnswer = new PlayerTwoInputSelections(userTwoInputSelections);
+                Console.ReadLine();
+            }
+        }
+    }
+}
